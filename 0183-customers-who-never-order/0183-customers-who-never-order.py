f138a1b79042ba8f232ba60270d36baaf96a1ca6
Martin-Regan <martin.regan909@gmail.com>
2026-06-12 16:52:21 +0100

    Time: 316 ms (11.85%), Space: 67.5 MB (53.15%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    fixed = orders.join(customers.set_index("id"), on="customerId", how='right')
-    cleaned = fixed[fixed['id'].isna()]
-    cleaned.rename(columns={'name': 'Customers'}, inplace=True)
-    return cleaned[['Customers']]
+    condition = ~customers['id'].isin(orders['customerId'])
+    result = customers[condition]
+    return result[['name']].rename(columns={'name': 'customers'})
